c83773b1c14dcca63775e14a8abe57d34974f302
Richard <skynet.tw@gmail.com>
2022-07-07 11:43:40 +0800

    add News class and table

--- mysite/models.py
+++ mysite/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+class News(models.Model):
+    title = models.CharField(max_length=200)
+    content = models.TextField()
+    pdate = models.CharField(max_length=50)
+    url = models.CharField(max_length=200)
+
+    class Meta:
+        ordering = ('-pdate',)
+
+    def __str__(self):
+        return self.title
+
