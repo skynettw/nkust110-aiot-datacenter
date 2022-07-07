2fd250863ee910fe3910c266f13df2130c8d68f0
Richard <skynet.tw@gmail.com>
2022-07-07 11:29:53 +0800

    add lotto

--- datacenter/urls.py
+++ datacenter/urls.py
@@ -3,6 +3,7 @@ from django.urls import path
 from mysite import views
 
 urlpatterns = [
+    path('lotto/', views.lotto),
     path('', views.index),
     path('admin/', admin.site.urls),
 ]
--- mysite/views.py
+++ mysite/views.py
@@ -4,7 +4,10 @@ import random
 def index(request):
     name = "不分系何老師"
     
+    return render(request, "index.html", locals())
+
+def lotto(request):
     lotto = [i for i in range(1,50)]
     random.shuffle(lotto)
     lotto = lotto[:6]
-    return render(request, "index.html", locals())
+    return render(request, "lotto.html", locals())
