e55c7f0468e8943e5f0dd2bc6c63e4232490c0f1
Richard <skynet.tw@gmail.com>
2022-07-11 19:10:28 +0800

    fix allimporter.py

--- allimporter.py
+++ allimporter.py
@@ -1,11 +1,35 @@
 import requests
-import json
-target = ['2303', '2344', '2337']
+from datetime import datetime
+import json, time
+import django, os
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'datacenter.settings')
+django.setup()
+
+from mysite.models import Company, StockInfo
+
+target = ['2801', '2884', '2886', '2882', '2881']
 url = "https://www.twse.com.tw/exchangeReport/STOCK_DAY?response=json&date=20220{}01&stockNo={}"
 urls = list()
 for c in target:
     for i in range(1, 8):
-        urls.append(url.format(i, c))
-for u in urls[:1]:
-    data = json.loads(requests.get(u).text)
-    print(data)
+        temp_dict = dict()
+        temp_dict['code'] = c
+        temp_dict['url'] = url.format(i, c)
+        urls.append(temp_dict)
+for u in urls:
+    print(u)
+    data = json.loads(requests.get(u['url']).text)
+    c = Company.objects.get(code=u['code'])
+    stock_data = data['data']
+    for s in stock_data:
+        yy, mm, dd = s[0].split("/")
+        the_date = datetime(int(yy)+1911, int(mm), int(dd))
+        rec = StockInfo.objects.filter(company=c, dateinfo=the_date)
+        if len(rec)==0:
+            rec = StockInfo(company=c, dateinfo=the_date, 
+                            open_price=float(s[3].replace(",", "")), 
+                            close_price=float(s[6].replace(",", "")), 
+                            volume = int(s[8].replace(",", "")))
+            rec.save()
+            print(rec.company, rec.close_price)
+    time.sleep(5)
